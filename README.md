91cc3f018d5c9942021aaeb6ecf63e7c4b1ef086
R.I.Pienaar <rip@devco.net>
2019-06-11 10:33:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## Kasa Home Plug CLI tool
+
+A small utility to manage TP Link Kasa Homeplugs.
+
+## Usage
+
+Full details are available using `--help`, here are some example commands:
+
+```
+$ kasa-plug --plug dehumidifier-plug info
+TP-Link Kasa Plug 'Dehumidifier' @ dehumidifier-plug
+
+  Device:
+
+              Alias: Dehumidifier
+              Model: Smart Wi-Fi Plug With Energy Monitoring (HS110(UK))
+        Power State: On
+                LED: Off
+      Power On Time: 16 minutes 52 seconds (1012 seconds)
+
+  Versions:
+
+           Software: 1.5.7 Build 180806 Rel.135437
+           Hardware: 2.1
+
+  Network:
+
+        MAC Address: D8:0D:17:D8:54:6D
+    Network Address: dehumidifier-plug
+    Signal Strength: -75 dBm
+```
+
+```
+$ kasa-plug --plug dehumidifier-plug energy
+  Power State: On
+        Alias: Dehumidifier
+Power On Time: 17 minutes 34 seconds (1054 seconds)
+ Current Watt: 731.538 W
+          Amp: 3.094 A
+   Total Watt: 11.263 kWh
+         Volt: 238.192 V
+```
+
+```
+$ kasa-plug --plug dehumidifier-plug energy --json
+{"sw_ver":"1.5.7 Build 180806 Rel.135437","hw_ver":"2.1","type":"IOT.SMARTPLUGSWITCH","model":"HS110(UK)","mac":"D8:0D:17:D8:54:6D","dev_name":"Smart Wi-Fi Plug With Energy Monitoring","alias":"Dehumidifier","relay_state":1,"on_time":1159,"on_time_string":"19 minutes 19 seconds","active_mode":"none","feature":"TIM:ENE","updating":0,"rssi":-75,"led_off":0,"longitude_i":144395,"latitude_i":358544,"hwId":"0750E2C15BB77902833ABF45366B8E9A","fwId":"00000000000000000000000000000000","deviceId":"8006B2F62F673091ED1A642C0B5AA7541B26675C","oemId":"AB8C79FE7869756511CDC455BDFE41EA","ntc_state":0,"power_on":true,"power_off":false,"address":"dehumidifier-plug","voltage_mv":238932,"volt":238.932,"current_ma":3123,"current_amp":3.123,"power_mw":740772,"power_w":740.772,"total_wh":11285,"total_watt":11.285}
+```
+
+## Status
+
+It is usable and downloadable on the release page.  It uses a Go library [hs1xxplug](https://github.com/ripienaar/hs1xxplug) so you can build your own commands or extend that library to support additional features.
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/template"
+	"github.com/ripienaar/hs1xxplug"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	ipaddress  string
+	jsonFormat bool
+	hs1xx      *hs1xxplug.Plug
+)
+
+func main() {
+	app := kingpin.New("kasa-plug", "Manages TP-Link Kasa HS1xx Smart Plugs")
+	app.Flag("plug", "IP address or hostname of the plug").Required().StringVar(&ipaddress)
+
+	icmd := app.Command("info", "Retrieves device information")
+	icmd.Flag("json", "Show JSON output").BoolVar(&jsonFormat)
+
+	ecmd := app.Command("energy", "Retrieves energy usage information")
+	ecmd.Flag("json", "Show JSON output").BoolVar(&jsonFormat)
+
+	app.Command("on", "Turns the plug on")
+	app.Command("off", "Turns the plug off")
+	app.Command("reboot", "Reboots the plug")
+	app.Command("status", "Retrieves power state")
+
+	cmd := kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	hs1xx = hs1xxplug.NewPlug(ipaddress)
+
+	switch cmd {
+	case app.GetCommand("info").FullCommand():
+		info()
+
+	case app.GetCommand("on").FullCommand():
+		kingpin.FatalIfError(hs1xx.PowerOn(), "Could not power on")
+
+	case app.GetCommand("off").FullCommand():
+		kingpin.FatalIfError(hs1xx.PowerOff(), "Could not power off")
+
+	case app.GetCommand("reboot").FullCommand():
+		kingpin.FatalIfError(hs1xx.Reboot(), "Could not reboot")
+
+	case app.GetCommand("status").FullCommand():
+		status()
+
+	case app.GetCommand("energy").FullCommand():
+		energy()
+	}
+}
+
+func energy() {
+	type combined struct {
+		hs1xxplug.Info
+		hs1xxplug.Energy
+	}
+
+	energynfo, err := hs1xx.Energy()
+	kingpin.FatalIfError(err, "Could not retrieve plug information")
+
+	nfo, err := hs1xx.Info()
+	kingpin.FatalIfError(err, "Could not retrieve plug information")
+
+	data := combined{
+		Energy: *energynfo,
+		Info:   *nfo,
+	}
+
+	report := `  Power State: {{if .On}}On{{else}}Off{{end}}
+        Alias: {{.Alias}}
+Power On Time: {{.OnTime}} ({{.OnTimeSeconds}} seconds)
+ Current Watt: {{.PowerUseWatt}} W
+          Amp: {{.Amp}} A
+   Total Watt: {{.TotalWatt}} kWh
+         Volt: {{.Volt}} V
+`
+
+	if jsonFormat {
+		out, err := json.Marshal(data)
+		kingpin.FatalIfError(err, "Could not JSON encode data")
+		fmt.Printf(string(out))
+	} else {
+		tmpl, err := template.New("plug").Parse(report)
+		kingpin.FatalIfError(err, "Could not process template")
+
+		err = tmpl.Execute(os.Stdout, data)
+		kingpin.FatalIfError(err, "Could not process template")
+	}
+}
+
+func status() {
+	nfo, err := hs1xx.PowerState()
+	kingpin.FatalIfError(err, "Could not retrieve plug information")
+
+	if nfo == hs1xxplug.PowerOn {
+		fmt.Println("On")
+	} else if nfo == hs1xxplug.PowerOff {
+		fmt.Println("Off")
+	} else {
+		fmt.Println("Unknown")
+	}
+}
+func info() {
+	nfo, err := hs1xx.Info()
+	kingpin.FatalIfError(err, "Could not retrieve plug information")
+
+	report := `TP-Link Kasa Plug '{{.Alias}}' @ {{.Address}}
+
+  Device:
+
+              Alias: {{.Alias}}
+              Model: {{.DeviceName}} ({{.Model}})
+        Power State: {{if .On}}On{{else}}Off{{end}}
+                LED: {{if eq .LEDOff 0}}Off{{else}}On{{end}}
+      Power On Time: {{.OnTime}} ({{.OnTimeSeconds}} seconds)
+
+  Versions:
+
+           Software: {{.SoftwareVersion}}
+           Hardware: {{.HardwareVersion}}
+
+  Network:
+
+        MAC Address: {{.MAC}}
+    Network Address: {{.Address}}
+    Signal Strength: {{.SignalStrength}} dBm
+
+`
+
+	if jsonFormat {
+		out, err := json.Marshal(nfo)
+		kingpin.FatalIfError(err, "Could not JSON encode data")
+		fmt.Printf(string(out))
+	} else {
+		tmpl, err := template.New("plug").Parse(report)
+		kingpin.FatalIfError(err, "Could not process template")
+
+		err = tmpl.Execute(os.Stdout, nfo)
+		kingpin.FatalIfError(err, "Could not process template")
+	}
+}
